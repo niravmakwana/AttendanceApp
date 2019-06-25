485010b3b0f9e9da19d4ba9a463c6d9a7930a025
niravmakwana <42968978+niravmakwana@users.noreply.github.com>
2019-06-25 12:56:19 -0400

    Add files via upload

---
+++ Attendance/Attendance/Attendance/Views/AboutPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Attendance.Views
+{
+    
+    [DesignTimeVisible(false)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Attendance/Attendance/Attendance/Views/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Attendance.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btn_Login_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new MainPage());
+
+        }
+    }
+}
