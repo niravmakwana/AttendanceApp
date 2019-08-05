7306b0b74db3a1e80761974ea6e59f135cef3c62
niravmakwana <42968978+niravmakwana@users.noreply.github.com>
2019-08-05 18:38:56 -0400

    Add files via upload

---
+++ SharkIDAutomation/SharkID/AndroidQueries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SharkID
+{
+	public class AndroidQueries : ContentPage
+	{
+		public AndroidQueries()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
---
+++ SharkIDAutomation/SharkID/AppInitializer.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.UITest;
+
+namespace SharkID
+{
+	public static class AppInitializer
+	{
+		const string apiKey = "YOUR_API_KEY";
+		//const string apkPath = "../../../XamarinCRM.Android/bin/Release/com.xamarin.xamarincrm.Signed.apk";
+		const string apkPath = "com.companyname.StudentData-Signed.apk";
+		//const string appFile = "../../../XamarinCRM.iOS/bin/iPhoneSimulator/Debug/XamarinCRMiOS.app";
+		const string bundleId = "com.xamarin.xamarinsharkid";
+
+		public static IApp StartApp(Platform platform)
+		{
+			if (platform == Platform.Android)
+			{
+				return ConfigureApp
+					.Android
+					.EnableLocalScreenshots()
+					//.ApiKey(apiKey)
+					.ApkFile(apkPath)
+					.StartApp();
+			}
+			return ConfigureApp
+				.iOS
+				//.ApiKey(apiKey)
+				//.AppBundle(appFile)
+				.EnableLocalScreenshots()
+				.InstalledApp(bundleId)
+				.StartApp();
+		}
+	}
+}
